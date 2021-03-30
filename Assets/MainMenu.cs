47206be2ed6c2197fa127ed5fc4c0f17dff267de
Sushil Kumar <johnklutz.sk@gmail.com>
2021-03-30 14:06:35 +0100

    added play and quit buttons to main menu

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Loading Game scene");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Closing application");
+        Application.Quit();
+    }
+}
