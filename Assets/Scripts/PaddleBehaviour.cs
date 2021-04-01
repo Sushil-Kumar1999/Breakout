cf57160c75fa5d3de81bfa9ef5b1593049f7f32c
Sushil Kumar <johnklutz.sk@gmail.com>
2021-04-01 06:21:01 +0100

    add paddle behaviour script to move paddle horizontally. Movement through border walls is blocked

---
+++ Assets/Scripts/PaddleBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PaddleBehaviour : MonoBehaviour
+{
+    private Rigidbody2D paddleRigidBody;
+    [SerializeField]
+    private float paddleSpeed;
+
+    private void Awake()
+    {
+        paddleRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal"); //Get if Any Horizontal Keys pressed ((A or D) or (Left or Right) Buttons)
+        Vector2 movement = new Vector2(moveHorizontal, 0f); //Put moveHorizontal in a Vector2 Variable (x,y)...moveHorizontal will be the x axis
+        paddleRigidBody.velocity = movement * paddleSpeed;
+
+    }
+}
