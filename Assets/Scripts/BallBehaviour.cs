360c579f08a5e8e8c97173f5216236a7e3e77305
Sushil Kumar <johnklutz.sk@gmail.com>
2021-04-01 11:55:11 +0100

    laid red bricks and made bricks disappear if ball collides with them

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -34,35 +34,16 @@ public class BallBehaviour : MonoBehaviour
         {
             ballInPlay = true;
             ballRigidBody.AddForce(Vector2.up * ballLaunchForce);
-        }
-        
-        //if (Input.GetButtonDown("Jump") && ballIsMoving == false)
-        //{
-        //    transform.parent = null;    //Disconnect the Ball from the Paddle
-        //    ballIsMoving = true;      //The Ball is in play (is moving)
-        //    ballRigidBody.isKinematic = false;		//Uncheck the isKinematic in the Rigidbody 2D
-
-        //    if (Input.GetAxis("Horizontal") == 0f)      
-        //        ballRigidBody.AddForce(new Vector2(1f, ballVelocity));  
-        //    else if (Input.GetAxis("Horizontal") > 0f) 
-        //        ballRigidBody.AddForce(new Vector2(ballVelocity, ballVelocity)); 
-        //    else if (Input.GetAxis("Horizontal") < 0f)      
-        //        ballRigidBody.AddForce(new Vector2(-ballVelocity, ballVelocity));	
-        //}
+        }   
     }
 
-    //void OnCollisionEnter2D(Collision2D collision)
-    //{
-    //    if(collision.gameObject.tag == "Paddle")
-    //    {
-    //        if (Input.GetAxis("Horizontal") == 0f)
-    //            ballRigidBody.AddForce(new Vector2(1f, ballInitialVelocity));
-    //        else if (Input.GetAxis("Horizontal") > 0f)
-    //            ballRigidBody.AddForce(new Vector2(ballInitialVelocity, ballInitialVelocity));
-    //        else if (Input.GetAxis("Horizontal") < 0f)
-    //            ballRigidBody.AddForce(new Vector2(-ballInitialVelocity, ballInitialVelocity));
-    //    }
-    //}
+    public void OnCollisionEnter2D(Collision2D collider)
+    {
+        if (collider.transform.tag == "Brick")
+        {
+            Destroy(collider.gameObject);
+        }
+    }
 
     public void OnTriggerEnter2D(Collider2D collider)
     {
@@ -72,10 +53,5 @@ public class BallBehaviour : MonoBehaviour
             ballRigidBody.velocity = Vector2.zero;
             ballInPlay = false;
         }
-    }
-
-    private void Respawn()
-    {
-        transform.position = paddle.position;
     }
 }
