d2f7ed5476b643901296d4cae2f1896a5a1adde6
Sushil Kumar <johnklutz.sk@gmail.com>
2021-04-04 03:26:48 +0100

    able to load and save settings

---
+++ Assets/Models/Settings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Assets.Models
+{
+    [Serializable]
+    public class Settings
+    {
+        public bool backgroundMusic;
+        public bool paddleSfx; // ball hit paddle sound
+        public bool brickSfx; // ball hit brick sound
+    }
+}
---
+++ Assets/Utilities/FileUtilities.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class FileUtilities : MonoBehaviour
+{
+    public static void WriteToFile(string fileName, string json)
+    {
+        string filePath = GetFilePath(fileName);
+        FileStream fileStream = new FileStream(filePath, FileMode.Create);
+
+        using (StreamWriter writer = new StreamWriter(fileStream))
+        {
+            writer.Write(json);
+        }
+    }
+
+    public static string ReadFromFile(string fileName)
+    {
+        string json = "";
+        string filePath = GetFilePath(fileName);
+
+        if (File.Exists(filePath))
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                json = reader.ReadToEnd();
+            }
+        }
+        else
+        {
+            Debug.LogError($"File {fileName} at path {filePath} not found");
+        }
+
+        return json;
+    }
+
+    public static string GetFilePath(string fileName)
+    {
+        return Application.persistentDataPath + "/" + fileName;
+    }
+}
