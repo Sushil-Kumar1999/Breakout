9354db00794952954c6e5749a28d65fcfc6ba0de
Sushil Kumar <johnklutz.sk@gmail.com>
2021-04-04 09:18:22 +0100

    able to load settings to and save settings from settings menu

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,15 +1,27 @@
 ﻿using Assets.Data.Models;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     private DataManager<Settings> settingsManager;
     private Settings settings;
 
+    [SerializeField] private Toggle backgroundMusicToggle;
+    [SerializeField] private Toggle paddleSfxToggle;
+    [SerializeField] private Toggle brickSfxToggle;
+
     private void Awake()
     {
         settingsManager = new DataManager<Settings>("Settings.json");
         settings = settingsManager.Load();
+    }
+
+    private void Start()
+    {
+        backgroundMusicToggle.isOn = settings.backgroundMusic;
+        paddleSfxToggle.isOn = settings.paddleSfx;
+        brickSfxToggle.isOn = settings.brickSfx;
     }
 
     public void ToggleBallHitPaddleSfx(bool isEnabled)
