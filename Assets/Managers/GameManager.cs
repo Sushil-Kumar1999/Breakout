7f249f578f00a02de3e378c91ac2d94061bb3e95
Sushil Kumar <johnklutz.sk@gmail.com>
2021-04-06 09:34:33 +0100

    Load game panel done except continue button

--- Assets/Managers/GameManager.cs
+++ Assets/Managers/GameManager.cs
@@ -167,6 +167,7 @@ public class GameManager : MonoBehaviour
                                   ? savedGame.label
                                   : currentSaveFiles + "," + savedGame.label;
         PlayerPrefs.SetString("SAVE_FILES", updatedSaveFiles);
+        PlayerPrefs.Save();
         Debug.Log("Game saved");
     }
 
---
+++ Assets/Scripts/LoadGameMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadGameMenu : MonoBehaviour
+{
+    [SerializeField] private Dropdown saveFilesDropdown;
+    [SerializeField] private TextMeshProUGUI summaryText;
+
+    private List<string> saveFileNames;
+    private SavedGameManager savedGameManager;
+
+    private void Awake()
+    {
+        saveFileNames = new List<string>(PlayerPrefs.GetString("SAVE_FILES").Split(','));
+        savedGameManager = SavedGameManager.GetInstance();
+        savedGameManager.Load(saveFileNames[0]);
+    }
+
+    private void Start()
+    {
+        PopulateDropdownList();
+        UpdateSummaryText();
+    }
+
+    public void Dropdown_IndexChanged(int index)
+    {
+        savedGameManager.Load(saveFileNames[index]);
+        UpdateSummaryText();
+    }
+
+    public void RestoreGame()
+    {
+        Debug.Log("Restoring from save file " + SavedGameManager.SavedGame.label);
+    }
+
+    private void PopulateDropdownList()
+    {
+        saveFilesDropdown.AddOptions(new List<string>(saveFileNames));
+    }
+
+    private void UpdateSummaryText()
+    {
+        string text = $"Label: {SavedGameManager.SavedGame.label}\n\n" +
+                      $"Date: {SavedGameManager.SavedGame.saveTime.ToString("d/MM/yyyy")}\n\n" +
+                      $"Time: {SavedGameManager.SavedGame.saveTime.ToString("hh:mm:ss tt")}\n\n" +
+                      $"Score: {SavedGameManager.SavedGame.score}\n\n" +
+                      $"Lives remaining: {SavedGameManager.SavedGame.livesRemaining}\n\n";
+
+        summaryText.text = text;
+    }
+}
