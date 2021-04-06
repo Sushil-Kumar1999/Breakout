59ac5e64373244d975b7e3888bdd62319b2cfe79
Sushil Kumar <johnklutz.sk@gmail.com>
2021-04-06 15:09:09 +0100

    handle case if save files are not present on hard disk

--- Assets/Scripts/LoadGameMenu.cs
+++ Assets/Scripts/LoadGameMenu.cs
@@ -8,15 +8,27 @@ public class LoadGameMenu : MonoBehaviour
 {
     [SerializeField] private Dropdown saveFilesDropdown;
     [SerializeField] private TextMeshProUGUI summaryText;
+    [SerializeField] private Button continueButton;
 
     private List<string> saveFileNames;
     private SavedGameManager savedGameManager;
+    private bool saveFilesAbsent;
 
     private void Awake()
     {
-        saveFileNames = new List<string>(PlayerPrefs.GetString("SAVE_FILES").Split(','));
-        savedGameManager = SavedGameManager.GetInstance();
-        savedGameManager.Load(saveFileNames[0]);
+        string saveFilesValue = PlayerPrefs.GetString("SAVE_FILES", string.Empty);
+        saveFilesAbsent = string.IsNullOrEmpty(saveFilesValue);
+
+        if (saveFilesAbsent)
+        {
+            continueButton.gameObject.SetActive(false);
+        } 
+        else
+        {
+            saveFileNames = new List<string>(saveFilesValue.Split(','));
+            savedGameManager = SavedGameManager.GetInstance();
+            savedGameManager.Load(saveFileNames[0]);
+        }
     }
 
     private void Start()
@@ -27,8 +39,11 @@ public class LoadGameMenu : MonoBehaviour
 
     public void Dropdown_IndexChanged(int index)
     {
-        savedGameManager.Load(saveFileNames[index]);
-        UpdateSummaryText();
+        if (!saveFilesAbsent)
+        {
+            savedGameManager.Load(saveFileNames[index]);
+            UpdateSummaryText();
+        }
     }
 
     public void RestoreGame()
@@ -39,17 +54,32 @@ public class LoadGameMenu : MonoBehaviour
 
     private void PopulateDropdownList()
     {
-        saveFilesDropdown.AddOptions(new List<string>(saveFileNames));
+        if (saveFilesAbsent)
+        {
+            saveFilesDropdown.AddOptions(new List<string> { "No save file available" });
+        }
+        else
+        {
+            saveFilesDropdown.AddOptions(saveFileNames);
+        }
     }
 
     private void UpdateSummaryText()
     {
-        string text = $"Label: {SavedGameManager.SavedGame.label}\n\n" +
-                      $"Date: {SavedGameManager.SavedGame.saveTime.ToString("d/MM/yyyy")}\n\n" +
-                      $"Time: {SavedGameManager.SavedGame.saveTime.ToString("hh:mm:ss tt")}\n\n" +
-                      $"Score: {SavedGameManager.SavedGame.score}\n\n" +
-                      $"Lives remaining: {SavedGameManager.SavedGame.livesRemaining}\n\n";
+        if (!saveFilesAbsent)
+        {
+            string text = $"Label: {SavedGameManager.SavedGame.label}\n\n" +
+                          $"Date: {SavedGameManager.SavedGame.saveTime.ToString("d/MM/yyyy")}\n\n" +
+                          $"Time: {SavedGameManager.SavedGame.saveTime.ToString("hh:mm:ss tt")}\n\n" +
+                          $"Score: {SavedGameManager.SavedGame.score}\n\n" +
+                          $"Lives remaining: {SavedGameManager.SavedGame.livesRemaining}\n\n";
 
-        summaryText.text = text;
+            summaryText.text = text;
+        }
+        else
+        {
+            summaryText.text = "No save files available.\n\n" +
+                               "Start a new game and save your progress first.";
+        }
     }
 }
